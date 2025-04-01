ef7390e48d558e816f322ecbb6ad6ab0ed578902
Thomas Chen <thomas_q_chen@brown.edu>
2025-04-01 19:22:17 -0400

    partially updated services page

--- contentfulClient.js
+++ contentfulClient.js
@@ -6,20 +6,43 @@ const client = createClient({
 });
 
 async function displayEntries() {
-    try {
-      const entries = await client.getEntries({ content_type: 'recruitment' });
-  
-      const contentDiv = document.getElementById('content');
-      entries.items.forEach(entry => {
-        const article = document.createElement('article');
-        article.innerHTML = `<h2>${entry.fields.description}</h2><br>
-                            <p>${entry.fields.ccgEmail}</p>
-                             <p>${entry.fields.annualReport}</p><br>`;
-        contentDiv.appendChild(article);
-      });
-    } catch (error) {
-      console.error('Error fetching entries:', error);
-    }
+  try {
+    const entries = await client.getEntries({ content_type: 'recruitment' });
+
+    const contentDiv = document.getElementById('content');
+    entries.items.forEach(entry => {
+      const article = document.createElement('article');
+      article.innerHTML = `<h2>${entry.fields.description}</h2><br>
+                          <p>${entry.fields.ccgEmail}</p>
+                           <p>${entry.fields.annualReport}</p><br>`;
+      contentDiv.appendChild(article);
+    });
+  } catch (error) {
+    console.error('Error fetching entries:', error);
   }
-  
-  displayEntries();
+}
+
+//displayEntries();
+
+async function fetchImageById(entryId) {
+  try {
+    const entry = await client.getEntry(entryId);
+    console.log('Fetched Entry:', entry);
+
+    // Check if the entry has an "images" field
+    if (entry.fields.images && entry.fields.images.length > 0) {
+      // Extract the first image from the array
+      const imageUrl = `https:${entry.fields.images[0].fields.file.url}`;
+      
+      // Set the image URL in the HTML
+      document.getElementById('contentful-image').src = imageUrl;
+    } else {
+      console.error('No images found in this entry.');
+    }
+  } catch (error) {
+    console.error('Error fetching images:', error);
+  }
+}
+
+// // Call the function with a specific Entry ID
+fetchImageById('QD02xJjeDzhkY3Sm8bu1h');
