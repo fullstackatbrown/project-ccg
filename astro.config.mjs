bbc1831d94bdb841ba915ddb64accf4d9d2e657e
kdolgopo <144499797+kdolgopo@users.noreply.github.com>
2025-04-23 16:57:12 -0400

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: 'https://fullstackatbrown.github.io/project-ccg',
-  base: '/project-ccg/',
+  output: 'server',
+  adapter: vercel(),
 });
