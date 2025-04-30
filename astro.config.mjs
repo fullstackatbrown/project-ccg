a8ecf58f9a5f2d765f1c8c317fc3782d6aa7a383
kdolgopo <144499797+kdolgopo@users.noreply.github.com>
2025-04-30 15:38:34 -0400

    path change

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   site: "https://www.brownccg.com/",
-  base: "",
+  base: "brownccg.com",
   output: "static",
   build: {
     format: "directory",
